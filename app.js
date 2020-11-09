7bdb6ac619677a6fafc9a236ef422f994758d879
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-11-09 21:45:49 +0100

    Tweaks

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 let navIcon = document.getElementById("navIcon")
 navIcon.addEventListener("click", function(){
     navIcon.classList.toggle("open");
-})
+});
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
