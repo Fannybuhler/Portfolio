4a1963ee82b436f53bf29b6e50841515b782d1ec
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-11-11 17:59:45 +0100

    carousel working and lots of stuuf¨

--- app.js
+++ app.js
@@ -93,3 +93,10 @@ contactMe.addEventListener("click", function(){
 
     
 })(window);
+
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
